36deeaedfb30f0577bb332f12ee3de7308aa093f
suvobd100 <makbarbd@gmail.com>
2025-03-04 01:47:30 +0600

    all visas data fetch from db to allvisas page

--- src/pages/AllVisas.jsx
+++ src/pages/AllVisas.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Loading from './Loading';
 
 const AllVisas = () => {
+
+  
+  const [visasData, setVisasData] = useState(null); // all visas data
+  const [loading, setLoading] = useState(true); // handle loading
+  const [error, setError] = useState(null); // handle errors message
+
+  useEffect(() => {
+    // Fetch data from the backend API
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/visas');
+        if (!response.ok) {
+          throw new Error('Failed to fetch city data');
+        }
+        const data = await response.json();
+        setVisasData(data); // Set the fetched data
+      } catch (err) {
+        setError(err.message); // Set error message
+      } finally {
+        setLoading(false); // Set loading to false
+      }
+    };
+
+    fetchData();
+  }, []); 
+
+  if (loading) {
+    return <div><Loading/></div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!visasData) {
+    return <div>No data found </div>;
+  }
+
+
+
+
   return (
-    <div>AllVisas</div>
+    <div>No of All Visas: {visasData.length}  </div>
   )
 }
 
